56993a77e83a2c42d210c95137dd63052724178d
gamecattt <ak19gamecat@gmail.com>
2023-04-04 17:47:55 +0300

    pr15

--- backend/testToken.js
+++ backend/testToken.js
@@ -4,19 +4,10 @@ const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJjMWE0ZGY5N
 const SECRET_KEY_DEV = 'a2175b93e0227437d22d7cdf64ad93bb'; // вставьте сюда секретный ключ для разработки из кода
 try {
   jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-  console.log(
-    '\x1b[31m%s\x1b[0m',
-    `
-Надо исправить. В продакшне используется тот же
-секретный ключ, что и в режиме разработки.
-`
-  );
+  console.log('\x1b[31m%s\x1b[0m', `Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.`);
 } catch (err) {
   if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-    console.log(
-      '\x1b[32m%s\x1b[0m',
-      'Всё в порядке. Секретные ключи отличаются'
-    );
+    console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
   } else {
     console.log('\x1b[33m%s\x1b[0m', 'Что-то не так', err);
   }
